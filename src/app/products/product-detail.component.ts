f0e63d3eae485de211f813e4a01067fb2d09eacf
jstacey <janet.stacey@esr.cri.nz>
2022-08-16 16:24:36 +1200

    own code for course

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IProduct } from './product';
+import { ProductService } from "./product.service";
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+
+export class ProductDetailComponent implements OnInit {
+
+
+  pageTitle: string = "Product Detail";
+  products: IProduct[] = [];
+  product: any;
+  Detailsub!: Subscription;
+  errorMessage: string = "";
+
+  constructor(private route: ActivatedRoute, 
+              private router: Router, 
+              private productService: ProductService) { };
+
+  onBack(): void{
+    this.router.navigate(['/products']);
+  }
+  
+
+  ngOnInit(): void { 
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`
+    // this.product = this.route.snapshot.paramMap;
+    
+    console.log(this.product)
+    //this.productService.getProducts().subscribe({
+  //     next: products => {
+  //       // this.products = products;
+  //       this.product = this.products.find((item, routeId = id) => item.productId === routeId);
+  //       // console.log(`Chosen product: ${this.product.productName}`)
+  //      },
+  //     error: err => this.errorMessage = err
+  //   });
+   };
+
+
+  // ngOnDestroy(): void{
+  //   this.Detailsub.unsubscribe()
+  // }
+}
+
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
+
+@Component({
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit, OnDestroy {
+    
+    constructor(private productService: ProductService) {}
+;
+    
+    
+    pageTitle: string = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    products: IProduct[] = [];
+    filteredProducts: IProduct[] = [];
+    errorMessage: string = "";
+    sub!: Subscription;
+
+    private _listFilter: string = "";
+
+    get listFilter() : string {
+        return this._listFilter;
+    };
+
+    set listFilter(value:string){
+        this._listFilter = value;
+        console.log("filter set as: ", value);
+        this.filteredProducts = this.performFilter(value);
+    };
+    
+    ngOnInit(): void{
+        console.log("Loading...")
+        
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products,
+                this.filteredProducts  = this.products;
+             },
+            error: err => this.errorMessage = err
+        });
+        this.filteredProducts  = this.products;
+    };
+
+    toggleImage(): void{
+        this.showImage = !this.showImage;
+    }; 
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product:IProduct) =>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    };   
+    
+    onRatingClicked(message:string): void{
+        this.pageTitle = 'Product List: ' + message;
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
+   
+}
